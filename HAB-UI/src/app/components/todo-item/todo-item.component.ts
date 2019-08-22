3ca94538619fd6165a102422eb52dfeb4300bc44
whitewolfzzxx <mboyandsboy@gmail.com>
2019-08-22 16:12:50 -0400

    Needed to commit

---
+++ HAB-UI/src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+import { TodoService } from '../../services/todo.service';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+
+  constructor(private todoService:TodoService) { }
+
+  ngOnInit() {
+  }
+
+  // Set Dynamic Classes
+  setClass() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+    return classes;
+  }
+
+  onToggle(todo) {
+    // Toggle UI
+    todo.completed = !todo.completed;
+    // Toggle Server
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
+  }
+
+  onDelete(todo) {
+    console.log("delete");
+  }
+}

9241cc30a1049b7c26bb73793d5638918da9f480
whitewolfzzxx <mboyandsboy@gmail.com>
2019-08-22 17:58:39 -0400

    Removed carousel

--- HAB-UI/src/app/app.module.ts
+++ HAB-UI/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    CarouselComponent
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
